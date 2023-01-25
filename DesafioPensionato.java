98a478bbbe09a554bd90c6130cf16bbbbf3ae64d
JonasBife <jonas.spacheco@gmail.com>
2023-01-24 21:48:08 -0300

    Formatação inicial da classe e for dos inputs de estudantes

--- DesafioPensionato.java
+++ DesafioPensionato.java
@@ -7,10 +7,23 @@ public class DesafioPensionato {
     Scanner leitor = new Scanner(System.in);
     int[] quartos = new int[10];
 
-    System.out.println("Digite o número de estudantes que alugarão quartos");
-    int i = leitor.nextInt();
-    
+    System.out.print("Digite o número de estudantes que alugarão quartos: ");
+    int N = leitor.nextInt();
+    leitor.nextLine();
+    Estudante[] student = new Estudante[N];
+    for(int i =0;i<N;i++){
+        System.out.println("Está sendo realizado o registro do estudante " + (i+1));
+        student[i] = new Estudante();
+        System.out.print("Digite o nome do estudante: ");
+        student[i].setNome(leitor.nextLine());
+        System.out.print("Digite o e-mail do estudante: ");
+        student[i].setEmail(leitor.nextLine());
+        System.out.print("Digite o quarto que deseja se hospedar: ");
+        student[i].setQuarto(leitor.nextInt());
+        leitor.nextLine();
+        System.out.println(student[i]);
+    }
 
-
+    leitor.close();
     }
 }
--- Estudante.java
+++ Estudante.java
@@ -2,10 +2,15 @@ public class Estudante {
 
     private String nome;
     private String email;
+    private int quarto;
 
-    public Estudante(String nome, String email){
+    public Estudante(){
+
+    }
+    public Estudante(String nome, String email, int quarto){
         this.nome = nome;
         this.email = email;
+        this.quarto = quarto;
     }
 
     public String getNome() {
@@ -23,4 +28,21 @@ public class Estudante {
     public void setEmail(String email) {
         this.email = email;
     }
+
+    public int getQuarto(){
+        return quarto;
+    }
+
+    public void setQuarto(int quarto){
+        this.quarto=quarto;
+    }
+
+    //Formatação para printar em tela
+    public String toString() {
+        return "Nome: " + nome
+                +"\nEmail: " + email
+                +"\nQuarto: " + quarto;
+    }
+
+
 }
