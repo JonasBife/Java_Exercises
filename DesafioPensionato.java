b83d65c62ff10ed682bef6d2da858650db3372fa
JonasBife <jonas.spacheco@gmail.com>
2023-01-24 21:03:18 -0300

    Criação do desafio do pensionato com as classes main e Estudante

---
+++ DesafioPensionato.java
@@ -0,0 +1,16 @@
+/*Desafio Pensionato*/
+
+import java.util.Scanner;
+
+public class DesafioPensionato {
+    public static void main(String[] args) {
+    Scanner leitor = new Scanner(System.in);
+    int[] quartos = new int[10];
+
+    System.out.println("Digite o número de estudantes que alugarão quartos");
+    int i = leitor.nextInt();
+    
+
+
+    }
+}
---
+++ Estudante.java
@@ -0,0 +1,26 @@
+public class Estudante {
+
+    private String nome;
+    private String email;
+
+    public Estudante(String nome, String email){
+        this.nome = nome;
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
